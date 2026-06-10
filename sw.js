00e626b9f03d6a3861df71119479a253e9c072e8
tobfm51-pixel <tobfm51@gmail.com>
2026-06-10 11:08:18 -0400

    Add active case access guard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fip-field-notes-alpha-05-v1';
+const CACHE = 'fip-field-notes-alpha-12-v1';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
